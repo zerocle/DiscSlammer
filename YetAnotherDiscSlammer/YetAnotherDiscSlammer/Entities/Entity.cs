32316fa723b50a8b48710af30b1466c1c202fe68
The Janitor <eljanitor@skin.io>
2013-11-17 23:23:38 +0000

    Moving to a base entity class

---
+++ YetAnotherDiscSlammer/YetAnotherDiscSlammer/Entities/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using YetAnotherDiscSlammer.Common;
+
+namespace YetAnotherDiscSlammer.Entities
+{
+   public abstract class Entity
+   {
+      /// <summary>
+      /// Entity types are used in collision calculations.
+      /// </summary>
+      public String EntityType { get; protected set; }
+      public Vector2 Position { get; set; }
+      public virtual Rectangle BoundingRectangle
+      {
+         get;
+      }
+
+      public Entity(Vector2 Position, String EntityType)
+      {
+         this.EntityType = EntityType;
+         this.Position = Position;
+      }
+   }
+}
